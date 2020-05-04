df8492d1a315e8a94b4cdab553ea572198458bc6
abol <abolfazlsadeqi2001@chmail.ir>
2020-05-04 10:33:34 +0430

    problem resolving on play received sound + refactoring#worked

--- drello/src/main/webapp/stream_receiver/sound_receiver.js
+++ drello/src/main/webapp/stream_receiver/sound_receiver.js
@@ -1,25 +1,40 @@
-var soundWS;
+var soundWS = new WebSocket(soundURL);
 var isSetCurrentTime = false;
 var currentTime = 0;
 var array = [];
 var audio;
+
 function start() {
-	soundWS = new WebSocket(soundURL);
 	audio = document.querySelector("audio");
-	soundWS.onclose = function(){
-		location.href = mainPage;
-	}
-	soundWS.onmessage = function(e) {
-		if (typeof e.data === "string") {
-			// e.data contain number of blobs * each blob time duration = current time
-			currentTime = Number(e.data);
-			// prevent set default current time
-			isSetCurrentTime = true;
-		} else {
-			play(e.data);
-		}
-	};
 }
+
+soundWS.onmessage = function(e) {
+	if (typeof e.data === "string") {
+		// e.data contain number of blobs * each blob time duration = current time
+		currentTime = Number(e.data);
+		// prevent set default current time
+		isSetCurrentTime = true;
+	} else {
+		play(e.data);
+	}
+}
+
+soundWS.onclose = function(){
+	location.href = mainPage;
+}
+
+function play(data) {
+	// set current time
+	if (!isSetCurrentTime) {
+		currentTime = audio.currentTime;
+	} else {
+		isSetCurrentTime = false;
+	}
+	// set url based on blobs
+	var url = getURL(data);
+	audio.src = url;
+}
+
 function getURL (data){
 	// push new blob to array
 	array.push(data)
@@ -29,28 +44,18 @@ function getURL (data){
 	});
 	return URL.createObjectURL(blob);
 }
-function play(data) {
-	var url = getURL(data);
-	// set current time
-	if (!isSetCurrentTime) {
-		currentTime = audio.currentTime;
-	} else {
-		isSetCurrentTime = false;
-	}
-	
-	audio.src = url;
-}
+
 function loadNewMusic() {
+	audio.pause();
 	if (audio.duration > currentTime) {
+		audio.playbackRate = 2;
 		audio.currentTime = currentTime;
-		if (audio.paused) {
-			audio.play();
-		}
+		audio.play();
 	}else{
 		isSetCurrentTime = true;
-		audio.pause();
 	}
 }
+
 function finishAudio() {
 	// when audio finished the cursor go to 0 but after receiving new sound makes duration longer than so we need to save the current time to prevent start at beginning
 	currentTime = audio.duration;
